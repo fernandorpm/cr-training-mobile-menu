9c89b134c07b91ecc288cc8b0cc385ba1e418983
Fernando R P Marques <frpm1001@gmail.com>
2021-10-01 18:36:49 -0300

    Add linters and fix code according to linters

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-const buttonOpenMenuMobile = document.querySelector('#open-menu-mobile');
-const buttonCloseMenuMobile = document.querySelector('#close-menu-mobile');
 const menuMobile = document.querySelector('#mobile-nav');
 
+// eslint-disable-next-line no-unused-vars
 function openMenuMobile() {
   menuMobile.style.display = 'block';
 }
 
+// eslint-disable-next-line no-unused-vars
 function closeMenuMobile() {
   menuMobile.style.display = 'none';
 }
