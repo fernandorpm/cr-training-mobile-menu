2a6487e3139b50909a5c0112b6aa77a8472a393d
Fernando R P Marques <frpm1001@gmail.com>
2021-10-01 18:10:54 -0300

    Add Mobile menu according to the Figma Reference

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const buttonOpenMenuMobile = document.querySelector('#open-menu-mobile');
+const buttonCloseMenuMobile = document.querySelector('#close-menu-mobile');
+const menuMobile = document.querySelector('#mobile-nav');
+
+function openMenuMobile() {
+  menuMobile.style.display = 'block';
+}
+
+function closeMenuMobile() {
+  menuMobile.style.display = 'none';
+}
